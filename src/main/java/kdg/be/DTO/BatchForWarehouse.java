dc157ba2edc53dd5f1694e210ce31d6fadb5709c
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-05-16 14:05:14 +0000

    Update 52 files
    - /src/main/java/EnkeleBemerkingen.MD
    - /src/main/java/img.png
    - /src/main/java/kdg/be/BakkeryTijdelijkApplication.java
    - /src/main/java/kdg/be/Configuratie.java
    - /src/main/java/kdg/be/Demo13Application.java
    - /src/main/java/kdg/be/Controllers/BatchController.java
    - /src/main/java/kdg/be/Controllers/IngredientController.java
    - /src/main/java/kdg/be/Controllers/LegeFile
    - /src/main/java/kdg/be/Controllers/ProductController.java
    - /src/main/java/kdg/be/Controllers/TestController.java
    - /src/main/java/kdg/be/Controllers/ToDO.MD
    - /src/main/java/kdg/be/Deserializers/IngredientDeserializer.java
    - /src/main/java/kdg/be/DTO/BatchForWarehouse.java
    - /src/main/java/kdg/be/DTO/IngredientDTO.java
    - /src/main/java/kdg/be/DTO/OrdersFromClientDTO.java
    - /src/main/java/kdg/be/Managers/BatchManager.java
    - /src/main/java/kdg/be/Managers/BatchProductManager.java
    - /src/main/java/kdg/be/Managers/IBatchManager.java
    - /src/main/java/kdg/be/Managers/IBatchproductManager.java
    - /src/main/java/kdg/be/Managers/IIngredientManager.java
    - /src/main/java/kdg/be/Managers/IngredientManager.java
    - /src/main/java/kdg/be/Managers/IProductManager.java
    - /src/main/java/kdg/be/Managers/ProductManager.java
    - /src/main/java/kdg/be/Models/Batch.java
    - /src/main/java/kdg/be/Models/BatchProduct.java
    - /src/main/java/kdg/be/Models/BatchState.java
    - /src/main/java/kdg/be/Models/Ingredient.java
    - /src/main/java/kdg/be/Models/PreparationState.java
    - /src/main/java/kdg/be/Models/Product.java
    - /src/main/java/kdg/be/Models/ProductState.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitConsumer.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitMQConfiguratie.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitSender.java
    - /src/main/java/kdg/be/Repositories/IBatchproductRepository.java
    - /src/main/java/kdg/be/Repositories/IBatchRepository.java
    - /src/main/java/kdg/be/Repositories/IIngredientRepository.java
    - /src/main/java/kdg/be/Repositories/IProductRepository.java
    - /src/main/java/kdg/be/Services/BatchSerializer.java
    - /src/main/java/kdg/be/Services/JsonSerializer.java
    - /src/main/java/kdg/be/Services/OrderFromClientDeserializer.java
    - /src/main/java/kdg/be/Validator/ProductValidator.java
    - /src/main/resources/application.properties
    - /src/main/resources/errorPagina.html
    - /src/main/resources/javascript/navigationMenu.js
    - /src/main/resources/templates/NotFound.html
    - /src/main/resources/templates/Batchen/Batch.html
    - /src/main/resources/templates/Ingredienten/IngredientOverview.html
    - /src/main/resources/templates/Products/newProduct.html
    - /src/main/resources/templates/Products/productDetail.html
    - /src/main/resources/templates/Products/ProductenOverzicht.html
    - /src/test/java/kdg/be/BakkeryTijdelijkApplicationTests.java
    - /src/test/java/kdg/be/Demo13ApplicationTests.java

---
+++ src/main/java/kdg/be/DTO/BatchForWarehouse.java
@@ -0,0 +1,103 @@
+package kdg.be.DTO;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import kdg.be.Models.Batch;
+import kdg.be.Models.Ingredient;
+import kdg.be.Models.Product;
+import kdg.be.Services.BatchSerializer;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@JsonSerialize(using = BatchSerializer.class)
+public class BatchForWarehouse {
+    List<Ingredient> ingredients=new ArrayList<>();
+    List<Double>Amounts=new ArrayList<>();
+
+    Long batchId;
+
+    LocalDate batchDate;
+
+    public BatchForWarehouse() {
+    }
+
+    public LocalDate getBatchDate() {
+        return batchDate;
+    }
+
+    public void setBatchDate(LocalDate batchDate) {
+        this.batchDate = batchDate;
+    }
+
+    public BatchForWarehouse(List<Ingredient> ingredients, List<Double> amounts, Long batchId) {
+        this.ingredients = ingredients;
+        this.Amounts = amounts;
+        this.batchId = batchId;
+    }
+
+    public BatchForWarehouse crateBathForWaehouse(Batch batch){
+
+
+        Map<Ingredient,Double> toSend=new HashMap<>();
+
+        Map<Product,Integer>products= batch.getProductsinBatch();
+        //Voor elk product dat besteld is...
+        products.forEach((product,numberOfProducts)->{
+            List<Ingredient> ingredients=  product.getComposition();
+            List<Double>  amounts=product.getAmounts();
+            List<Double> ingredientMultiplyNumber=amounts.stream().map(i->i*numberOfProducts).toList();
+            ingredients.forEach(ingr->{
+
+
+                for(int i=0;i<ingredients.size();i++){
+
+                    toSend.putIfAbsent(ingredients.get(i),0.0);
+
+                    toSend.put(ingredients.get(i),
+                            toSend.get(ingredients.get(i))+ingredientMultiplyNumber.get(i));
+
+
+                }
+
+
+            });
+
+
+
+        });
+        return new BatchForWarehouse(toSend.keySet().stream().toList(),toSend.values().stream().toList(),1L);
+
+
+
+
+
+    }
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Double> getAmounts() {
+        return Amounts;
+    }
+
+    public void setAmounts(List<Double> amounts) {
+        Amounts = amounts;
+    }
+
+    public Long getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Long batchId) {
+        this.batchId = batchId;
+    }
+}
---
+++ src/main/java/kdg/be/Validator/ProductValidator.java
@@ -0,0 +1,96 @@
+package kdg.be.Validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import jakarta.validation.ConstraintViolation;
+import kdg.be.Models.Ingredient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import jakarta.validation.executable.ExecutableValidator;
+import jakarta.validation.metadata.BeanDescriptor;
+import kdg.be.Models.Product;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+@Qualifier("ProductValidator")
+public class ProductValidator implements Validator {
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Product.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object pr, Errors errors) {
+
+        Product product= (Product) pr;
+        if(product.getName().length()<3){
+
+            errors.rejectValue("name","",new Object[]{"'name'"},"A name should at least be three letters long");
+        }
+
+        if(product.getSteps().stream().filter(a->a.length()!=0).toList().size()<3){
+
+            errors.rejectValue("steps","",new Object[]{"'composition'"},"A valid recepy has three steps");
+        }
+       if(product.getAmounts().stream().anyMatch(a->a<=0)){
+
+            errors.rejectValue("amounts","",new Object[]{"'amounts'"},"Quantities have to be positive numbers");
+        }
+        if(product.getComposition().size()<2){
+
+            errors.rejectValue("composition","",new Object[]{"'composition'"},"Products should consist of at least 2 ingredients");
+        }
+        else {
+
+            Map<Ingredient, Integer> m = new HashMap<>();
+            product.getComposition().forEach(p -> {
+                if (m.containsKey(p)) {
+                    int k = m.get(p) + 1;
+                    m.replace(p, k);
+
+                } else {
+                    m.put(p, 1);
+                }
+
+
+            });
+
+
+
+            Map<Ingredient,Integer> ing=m.entrySet().stream().filter((k)->k.getValue()>1).collect(Collectors.toMap(e->e.getKey(),e->e.getValue()));
+
+            if(ing.size()>0){
+
+                errors.rejectValue("composition","",new Object[]{"'composition'"},"Some ingredients are mentioned twice");
+
+            }
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public Errors validateObject(Object pr) {
+
+
+
+        return Validator.super.validateObject(pr);
+    }
+}
