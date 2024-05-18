298537f518aabb1c66bd49d042a8bf61033d50dc
Eliah Verbeemen <eliah.verbeemen@student.kdg.be>
2024-05-18 10:34:27 +0000

    Update 62 files
    - /src/main/resources/javascript/navigationMenu.js
    - /src/main/resources/templates/Batchen/Batch.html
    - /src/main/resources/templates/Ingredienten/IngredientOverview.html
    - /src/main/resources/templates/Products/ProductenOverzicht.html
    - /src/main/resources/templates/Products/newProduct.html
    - /src/main/resources/templates/Products/productDetail.html
    - /src/test/java/kdg/be/BakkeryTijdelijkApplicationTests.java
    - /src/test/java/kdg/be/Demo13ApplicationTests.java
    - /src/main/resources/templates/NotFound.html
    - /src/main/java/kdg/be/Controllers/BatchController.java
    - /src/main/java/kdg/be/Controllers/IngredientController.java
    - /src/main/java/kdg/be/Controllers/LegeFile
    - /src/main/java/kdg/be/Controllers/ProductController.java
    - /src/main/java/kdg/be/Controllers/TestController.java
    - /src/main/java/kdg/be/Controllers/ToDO.MD
    - /src/main/java/kdg/be/DTO/BatchForWarehouse.java
    - /src/main/java/kdg/be/DTO/IngredientDTO.java
    - /src/main/java/kdg/be/DTO/OrdersFromClientDTO.java
    - /src/main/java/kdg/be/Deserializers/IngredientDeserializer.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitConsumer.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitMQConfiguratie.java
    - /src/main/java/kdg/be/RabbitMQ/RabbitSender.java
    - /src/main/java/kdg/be/Repositories/IBatchRepository.java
    - /src/main/java/kdg/be/Repositories/IBatchproductRepository.java
    - /src/main/java/kdg/be/Repositories/IIngredientRepository.java
    - /src/main/java/kdg/be/Repositories/IProductRepository.java
    - /src/main/java/kdg/be/Repositories/IPurchaseOrderRepository.java
    - /src/main/java/kdg/be/Repositories/IPurchaseProductRepository.java
    - /src/main/java/kdg/be/Services/BatchSerializer.java
    - /src/main/java/kdg/be/Services/JsonSerializer.java
    - /src/main/java/kdg/be/Services/OrderFromClientDeserializer.java
    - /src/main/java/kdg/be/Services/PurchaseOrderDeserializer.java
    - /src/main/java/kdg/be/Services/PurchaseProductDeserializer.java
    - /src/main/java/kdg/be/Validator/ProductValidator.java
    - /src/main/resources/application.properties
    - /src/main/resources/errorPagina.html
    - /src/main/java/kdg/be/Managers/Repositories/IBatchManager.java
    - /src/main/java/kdg/be/Managers/Repositories/IBatchproductManager.java
    - /src/main/java/kdg/be/Managers/Repositories/IIngredientManager.java
    - /src/main/java/kdg/be/Managers/Repositories/IProductManager.java
    - /src/main/java/kdg/be/Managers/Repositories/IPurchaseOrder.java
    - /src/main/java/kdg/be/Managers/Repositories/IPurchaseOrderProduct.java
    - /src/main/java/kdg/be/Models/PurchaseOrder/PurchaseOrder.java
    - /src/main/java/kdg/be/Models/PurchaseOrder/PurchaseProduct.java
    - /src/main/java/kdg/be/Managers/BatchManager.java
    - /src/main/java/kdg/be/Managers/BatchProductManager.java
    - /src/main/java/kdg/be/Managers/IngredientManager.java
    - /src/main/java/kdg/be/Managers/ProductManager.java
    - /src/main/java/kdg/be/Managers/PurchaseOrderManager.java
    - /src/main/java/kdg/be/Managers/PurchaseOrderProductManager.java
    - /src/main/java/kdg/be/Models/Batch.java
    - /src/main/java/kdg/be/Models/BatchProduct.java
    - /src/main/java/kdg/be/Models/BatchState.java
    - /src/main/java/kdg/be/Models/Ingredient.java
    - /src/main/java/kdg/be/Models/PreparationState.java
    - /src/main/java/kdg/be/Models/Product.java
    - /src/main/java/kdg/be/Models/ProductState.java
    - /src/main/java/kdg/be/BakkeryTijdelijkApplication.java
    - /src/main/java/kdg/be/Configuratie.java
    - /src/main/java/kdg/be/Demo13Application.java
    - /src/main/java/EnkeleBemerkingen.MD
    - /src/main/java/img.png

--- src/main/java/kdg/be/Managers/Repositories/IBatchproductManager.java
+++
@@ -1,9 +0,0 @@
-package kdg.be.Managers.Repositories;
-
-import kdg.be.Models.BatchProduct;
-
-public interface IBatchproductManager {
-
-    public  BatchProduct save(BatchProduct batchProduct);
-
-}
--- src/main/java/kdg/be/Repositories/IPurchaseProductRepository.java
+++
@@ -1,7 +0,0 @@
-package kdg.be.Repositories;
-
-import kdg.be.Models.PurchaseOrder.PurchaseProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPurchaseProductRepository extends JpaRepository<PurchaseProduct,Long> {
-}
